9ced188e555d922730f3e6a6d81b3c3d61d83471
beta <beta@hai.haus>
2023-08-19 01:16:29 -0700

    Added (basic) encryption

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
+/*
+ * Hey there, thank you for reading the code to make sure that it's safe
+ * and that you can trust it! If you see anything alarming, please open
+ * an discussion and let me know, alternatively (if you don't want to
+ * create an account) you can email me at beta@hai.haus.
+ *
+ * ~ Daniel
+ */
+
 package main
 
 import (
+  "io/ioutil"
   "io"
   "os"
 
   "archive/tar"
   "compress/gzip"
+  
+  "crypto/aes"
+  "crypto/rand"
+  "crypto/cipher"
   
   //tea "github.com/charmbracelet/bubbletea"
   "github.com/charmbracelet/log"
@@ -16,7 +30,65 @@ func main() {
   if err != nil {
     log.Fatal("Failed to create archive.", "err", err)
   }
+
+  err = encryptFile("files.tar.gz")
+  if err != nil {
+    log.Fatal("Failed to encrypt files.", "err", err)
+  }
+
+  err = os.Remove("files.tar.gz")
+  if err != nil {
+    log.Fatal("Failed to delete archive.", "err", err)
+  }
+
 }
+
+/*
+ * encryptFile
+ * Encrypts a file
+ *
+ * Takes the file to encrypt
+ */
+func encryptFile(file string) error {
+  // Read the file and get contents
+  value, err := ioutil.ReadFile(file)
+  if err != nil {
+    return err
+  }
+  
+  // Create a block cipher
+  // SEE: https://en.wikipedia.org/wiki/Block_cipher
+  // TODO: Add actual key support
+  block, err := aes.NewCipher([]byte("UTFt3YBbdTFnf5wA"))
+  if err != nil {
+    return err
+  }
+  
+  // And now use GCM (Galois/Counter Mode)
+  // SEE: https://en.wikipedia.org/wiki/Galois/Counter_Mode
+  gcm, err := cipher.NewGCM(block)
+  if err != nil {
+    return err
+  }
+  
+  // Generate a random number (nonce)
+  nonce := make([]byte, gcm.NonceSize())
+  if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+    return err
+  }
+  
+  // Use the seal function (which encrypts and authenticates plaintext).
+  cipherText := gcm.Seal(nonce, nonce, value, nil)
+  
+  // Write the file out
+  err = ioutil.WriteFile(file + ".grave", cipherText, 0777)
+  if err != nil {
+	  return err
+  }
+
+  return nil
+}
+
 
 /*
  * makeArchive
@@ -25,11 +97,13 @@ func main() {
  * Takes the name of the directory to archive.
  */
 func makeArchive(dir string) error {
+  // Find all the files that need to be added to the archive.
   entries, err := os.ReadDir(dir)
   if err != nil {
     return err
   }
   
+  // Create a file that ends in `.tar.gz`
   buf, err := os.Create(dir + ".tar.gz")
   if err != nil {
     return err
@@ -79,3 +153,5 @@ func makeArchive(dir string) error {
 
   return nil
 }
+
+
